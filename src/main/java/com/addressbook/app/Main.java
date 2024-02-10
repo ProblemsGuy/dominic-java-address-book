5f2b9d4e0f0107db36461c51b28fbe0b8f6342e8
Dominic Fazakerley <dominic@fazakerley.org>
2024-02-10 15:05:52 +0000

    Added Main Class

---
+++ src/main/java/com/addressbook/app/Main.java
@@ -0,0 +1,35 @@
+package com.addressbook.app;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+    public Main(){}
+
+    public void menu(Scanner scanner, AddressBook book){
+
+    }
+
+    private void addContact(){
+
+    }
+
+    private void removeContact(){
+
+    }
+
+    private void editName(){
+
+    }
+
+    private void editNumber(){
+
+    }
+
+    private void printAddressBook(){
+
+    }
+}
