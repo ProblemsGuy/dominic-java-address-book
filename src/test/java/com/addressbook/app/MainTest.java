b6cc4dc84e41353ef3b0d6d41d125e26a194548a
Dominic Fazakerley <dominic@fazakerley.org>
2024-02-10 18:20:12 +0000

    Test added for duplicate data

--- src/test/java/com/addressbook/app/MainTest.java
+++ src/test/java/com/addressbook/app/MainTest.java
@@ -6,13 +6,12 @@ import org.junit.jupiter.api.Test;
 import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
 
 public class MainTest {
 
     @Test
     @DisplayName("Does `menu()` progress if it's given a '1' input, and can a user then add a contact?")
-    public void menuTest1(){
+    public void menuTest1() {
         //Arrange
         Main main = new Main();
         Scanner mockScanner = new Scanner("1 Jake jake@gmail.com 0777777778");
@@ -21,14 +20,14 @@ public class MainTest {
         main.menu(mockScanner);
 
         //Assert
-        assertEquals("Jake",main.book.getContactList().get(0).getName());
-        assertEquals("jake@gmail.com",main.book.getContactList().get(0).getNumber());
-        assertEquals("0777777778",main.book.getContactList().get(0).getEmail());
+        assertEquals("Jake", main.book.getContactList().get(0).getName());
+        assertEquals("jake@gmail.com", main.book.getContactList().get(0).getNumber());
+        assertEquals("0777777778", main.book.getContactList().get(0).getEmail());
     }
 
     @Test
     @DisplayName("Does `menu()` progress if it's given a '2' input, and can a user then remove a contact?")
-    public void menuTest2(){
+    public void menuTest2() {
         //Arrange
         Main main = new Main();
         Scanner scanner1 = new Scanner("1 Jake jake@gmail.com 0777777778");
@@ -39,12 +38,12 @@ public class MainTest {
         main.menu(scanner2);
 
         //Assert
-        assertEquals(0,main.book.getContactList().size());
+        assertEquals(0, main.book.getContactList().size());
     }
 
     @Test
     @DisplayName("Does trying to remove a contact through the `menu()` that doesn't exist warn the user?")
-    public void menuTest3(){
+    public void menuTest3() {
         //Arrange
         Main main = new Main();
         Scanner scanner1 = new Scanner("1 Jake jake@gmail.com 0777777778");
@@ -55,12 +54,12 @@ public class MainTest {
         main.menu(scanner2);
 
         //Assert
-        assertEquals(1,main.book.getContactList().size());
+        assertEquals(1, main.book.getContactList().size());
     }
 
     @Test
     @DisplayName("Does `menu()` progress if it's given a '3' input, and can a user then find a contact by name?")
-    public void menuTest4(){
+    public void menuTest4() {
         //Arrange
         Main main = new Main();
         Scanner scanner1 = new Scanner("1 Jake jake@gmail.com 0777777778");
@@ -76,7 +75,7 @@ public class MainTest {
 
     @Test
     @DisplayName("Does `editContact()` allow the user to edit a given Contact?")
-    public void menuTest5(){
+    public void menuTest5() {
         //Arrange
         Main main = new Main();
         Scanner scanner1 = new Scanner("1 Jake jake@gmail.com 0777777778");
@@ -91,9 +90,24 @@ public class MainTest {
         main.menu(scanner4);
 
         //Assert
-        assertEquals("Jim",main.book.getContactList().get(0).getName());
-        assertEquals("jim@gmail.com",main.book.getContactList().get(0).getNumber());
-        assertEquals("0799999998",main.book.getContactList().get(0).getEmail());
+        assertEquals("Jim", main.book.getContactList().get(0).getName());
+        assertEquals("jim@gmail.com", main.book.getContactList().get(0).getNumber());
+        assertEquals("0799999998", main.book.getContactList().get(0).getEmail());
+    }
 
+    @Test
+    @DisplayName("Does duplicate data not get input into the AddressBook when using `addContact()`")
+    public void menuTest6(){
+        //Arrange
+        Main main = new Main();
+        Scanner scanner1 = new Scanner("1 Jake jake@gmail.com 0777777778");
+        Scanner scanner2 = new Scanner("1 Jake jake@gmail.com 0777777778");
+
+        //Act
+        main.menu(scanner1);
+        main.menu(scanner2);
+
+        //Assert
+        assertEquals(1, main.book.getContactList().size());
     }
 }
